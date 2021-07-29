8ef717efeb0a605102a8da59dc71ab5ca79bddda
drsdesharma <drsdevsharma@gmail.com>
2021-07-29 22:51:48 +0530

    change setting

--- DoomCart/urls.py
+++ DoomCart/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('user.urls')),
     path('product/', include('Products.urls')),
     
-]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT) 
