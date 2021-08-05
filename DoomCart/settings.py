745ea4ec44b2954e9e9b7fc303c8890d4c695e56
drsdevsharma <47682316+drsdevsharma@users.noreply.github.com>
2021-08-05 20:08:45 +0530

    Email Verification

--- DoomCart/settings.py
+++ DoomCart/settings.py
@@ -28,7 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-l&h^ro@_yanee+_lze4#gss-^d-8bn&k2cw!s!c)85($q6hj4d'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
